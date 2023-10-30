ca9f69e871e92aa8b7a9531fec1cc41c6539808d
rhodinemma <rhodinemma10@gmail.com>
2023-10-30 14:05:17 +0300

    feat: writing tests with subsets

--- terminal-calculator/calculator/engine_test.go
+++ terminal-calculator/calculator/engine_test.go
@@ -37,14 +37,26 @@ func init() {
 func TestAdd(t *testing.T) {
 	// Arrange
 	e := calculator.Engine{}
-	x, y := 2.5, 3.5
-	want := 6.0
 
-	// Act
-	got := e.Add(2.5, 3.5)
+	actAssert := func(x, y, want float64) {
+		// Act
+		got := e.Add(x, y)
 
-	// Assert
-	if got != 6.0 {
-		t.Errorf("Add(%.2f,%.2f) incorrect, got: %.2f, want: %.2f.", x, y, got, want)
+		// Assert
+		if got != want {
+			t.Errorf("Add(%.2f,%.2f) incorrect, got: %.2f, want: %.2f.", x, y, got, want)
+		}
 	}
+
+	t.Run("positive input", func(t *testing.T) {
+		x, y := 2.5, 3.5
+		want := 6.0
+		actAssert(x, y, want)
+	})
+
+	t.Run("negative input", func(t *testing.T) {
+		x, y := -2.5, -3.5
+		want := -6.0
+		actAssert(x, y, want)
+	})
 }
